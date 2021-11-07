6cb15d0b8217a5476e34c6d2e0077503617c9337
Carlos Hoyos <choyos@cifpfbmoll.eu>
2021-11-07 21:40:56 +0100

    Crear clase MrMeeseks y su generador de Singleton

--- src/Meeseks/mr_meeseks.js
+++ src/Meeseks/mr_meeseks.js
@@ -1,1 +1,32 @@
 //State of original MrMeeseks
+
+function MrMeeseks() {
+    this.messageOnCreate = "I'm Mr Meeseeks! Look at meee!";
+    this.messageOnRequest = ["Oooh yeah! Can do!",
+                             "Yes sireeee!",
+                             "Oh, yeah!, Yes, ma'am!"];
+}
+
+
+
+
+//patrón Singleton para el MrMeeseeks original
+var generate = (function SingletonMrMeeseks() {
+    //creamos un nuevo objeto MrMeeseks dentro de la función
+    var prototipo = new MrMeeseks();
+    //esta funcion devuelve un getter de la instancia creada antes -> prototipo
+
+    return {
+        get: function() {
+            return prototipo;
+        }
+    };
+})();
+/**
+ * Hacemos de la función un IIFE para provocar la ejecución de esta y asegurar el
+ * estado del prototipo de esta primera instancia.
+ */
+
+//Exportación del módulo
+
+exports.singleMrMeeseks = generate;
--- src/Meeseks/test/mr_meeseks.test.js
+++ src/Meeseks/test/mr_meeseks.test.js
@@ -1,1 +1,17 @@
 //mrMeeseks test file
+const { expect } = require('@jest/globals');
+
+//Modulo MrMeeseks
+
+const generate = require('../mr_meeseks');
+
+test('Crear meeseks con su generador', () => {
+    expect(generate.singleMrMeeseks.get()).toBeTruthy();
+});
+
+test('Los meeseks generados son iguales (singleton)', () => {
+    let primer_meeseks = generate.singleMrMeeseks.get();
+    let segundo_meeseks = generate.singleMrMeeseks.get();
+
+    expect(segundo_meeseks === primer_meeseks).toBe(true);
+});
